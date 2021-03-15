3e99bd720df8c4ee909346452d3320f36d4564e0
vany pupkin <vany_pupkin@mail.ru>
2021-03-15 18:31:13 +0600

    add starting template

---
+++ src/js/modules/_hamburger.js
@@ -0,0 +1,10 @@
+
+//var hamburger = document.querySelector('.page-header__burger');
+//var mainNav = document.querySelector('.main-nav');
+//var pageBody = document.querySelector('.page__body');
+
+//hamburger.addEventListener('click', function() {
+//  hamburger.classList.toggle('active');
+//  mainNav.classList.toggle('main-nav--open');
+//  pageBody.classList.toggle('page__body--lock');
+//})
